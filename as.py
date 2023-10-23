95dbb48dd10776786cf3949cd111c67de641f4b8
Can Önal <can.onal@ug.bilkent.edu.tr>
2023-10-23 19:10:16 +0200

    change to pickle

---
+++ as.py
@@ -0,0 +1,7 @@
+from keras.models import load_model
+
+import pickle
+
+model = pickle.load(open('project_model.pkl','rb'))
+
+print(model.predict([[5.4,3.0,4.5,1.5]]))
--- prediction-ui-embedded/iris_predictor.py
+++ prediction-ui-embedded/iris_predictor.py
@@ -1,14 +1,15 @@
 import logging
+import pickle
 
 from keras.models import load_model
 
 
 class IrisPredictor:
     def __init__(self, model_file):
-        self.model = load_model(model_file)
+        self.model = pickle.load(open(model_file, 'rb'))
 
     def predict_single_record(self, df):
         y_pred = self.model.predict(df)
         logging.info("Prediction Output : %s", y_pred[0])
-        status = (y_pred[0] > 0.5)
+        status = (y_pred[0])
         return status
